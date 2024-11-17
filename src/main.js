4218000128fc69564d390410dffaed3fb9f04b1d
Nick  Seeley <nicholis@mail.com>
2024-11-17 13:49:23 -0800

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import '../styles/modern.normal.css' ;
+import '../styles/style.css' ;
+import '../styles/components/header.css'
+import '../styles/components/hero.css'
+import '../styles/utils.css' ;
+
